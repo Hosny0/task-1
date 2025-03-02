647a56cbb78133c2b39633a9695e00fbce689091
eng_hosny <eng_hosny@DESKTOP-CJDR7AH>
2025-03-03 01:43:38 +0200

    Add project files.

---
+++ task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcom to carpts cleaner");
+            double pricepersmallcarpt = 25.0;
+            double priceperlargecarpt = 35.0;
+            double taxvalue = 0.06;
+            Console.WriteLine("enter number of small carpts:");
+            int smallcarpt = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number of large carpts:");
+            int largecarpt = int.Parse(Console.ReadLine());
+            double cost = (smallcarpt * pricepersmallcarpt) + (largecarpt * priceperlargecarpt);
+            Console.WriteLine($"the cost:{cost}");
+            double tax = cost * taxvalue;
+            Console.WriteLine($"the tax:{tax}");
+
+            double totalestimate = cost + tax;
+            Console.WriteLine($"the totalestimate:{totalestimate}");
+            Console.WriteLine("this estimate is valid for 30 days");
+
+        }
+    }
+}
